1728f8caa7f82b9a99e016807c7635809ec3f6bd
José Mena Roldan <jose.mena@eurecat.org>
2018-06-13 10:56:15 +0200

    JMR: tensorboard viewers

--- tensorboard_viewer.py
+++ tensorboard_viewer.py
@@ -8,16 +8,15 @@ import csv
 variational_data = pickle.load(open('fits/playlists_songs_300D/variational.dat', 'rb'))
 playlist_embeddings = variational_data['rhos']
 playlist_sigmas = variational_data['sigma_rhos']
-discard_noise_indexes = np.where(playlist_sigmas > 0.05)
+discard_noise_indexes = np.where(playlist_sigmas > 0.5)
 good_embeddings = np.delete(playlist_embeddings, discard_noise_indexes, axis=0)
 
 # type = 'raw'|filtered
-type = 'raw'
+type = 'filtered'
 if type == 'raw':
     embeddings = playlist_embeddings
 else:
     embeddings = good_embeddings
-out_dir = type
 
 
 tf.reset_default_graph()
@@ -29,31 +28,31 @@ set_x = tf.assign(X, place, validate_shape=False)
 sess.run(tf.global_variables_initializer())
 sess.run(set_x, feed_dict={place: embeddings})
 
-os.makedirs(out_dir + '/' + type+'_emb_viz.log')
+out_dir = type
+if not os.path.isdir(out_dir):
+    os.makedirs(out_dir)
 # write labels
-with open(out_dir + '/' + type+'_emb_viz.log' + '/'+type+'_metadata.tsv', 'w') as f:
-    line_out = "title\n"
-    f.write(line_out)
-    with open('fits/playlists_songs_300D/dict_id_ntitle.csv', 'r') as dict_file:
-        dictionary_reader = csv.reader(dict_file, delimiter=',', quotechar='|')
+with open(out_dir + '/'+type+'_metadata.tsv', 'w') as f:
+    with open('fits/playlists_songs_300D/dict_id_ntitle_final.csv', 'r') as dict_file:
+        dictionary_reader = csv.reader(dict_file, delimiter=',')
         for row in dictionary_reader:
             if type == 'raw':
                 if row[0] != 'id':
                     line_out = "%s\n" % row[1]
                     f.write(line_out)
             else:
-                if row[0] != 'id' and not np.isin(int(row[0]), discard_noise_indexes):
-                    line_out = "%s\n" % row[1]
-                    f.write(line_out)
+                if row[0] != 'id':
+                    if not np.isin(int(row[0]), discard_noise_indexes[0]):
+                        line_out = "%s\n" % row[1]
+                        f.write(line_out)
 
 # create a TensorFlow summary writer
-#summary_writer = tf.summary.FileWriter(out_dir + '/' + 'filtered_emb_viz.log', sess.graph)
-summary_writer = tf.summary.FileWriter(out_dir + '/' + type+'_emb_viz.log', sess.graph)
+summary_writer = tf.summary.FileWriter(out_dir, sess.graph)
 config = projector.ProjectorConfig()
 embedding_conf = config.embeddings.add()
 embedding_conf.tensor_name = 'embedding:0'
-embedding_conf.metadata_path = os.path.join(out_dir + '/' + type+'_emb_viz.log', '../../'+type+'_metadata.tsv')
+embedding_conf.metadata_path = os.path.join(out_dir, '../'+type+'_metadata.tsv')
 projector.visualize_embeddings(summary_writer, config)
 
 saver = tf.train.Saver()
-saver.save(sess, os.path.join(out_dir + '/' + type+'_emb_viz.log', type+"_model.ckpt"))
+saver.save(sess, os.path.join(out_dir, type+"_model.ckpt"))
--- tensorboard_viewer_gensim.py
+++ tensorboard_viewer_gensim.py
@@ -7,39 +7,50 @@ import numpy as np
 import csv
 
 type = 'gensim'
-out_dir = type
-emb_file = '/Users/jose.mena/dev/personal/recsyschallenge18/embeddings/wv_model_normalized_playlists_size300_MPD'
-w2v_model = Word2Vec.load(emb_file)
-embeddings = w2v_model.wv.vectors
 
+emb_file_name = 'fits/playlists_songs_300D/wv_model_titles_MPD_final'
+dict_file_name = 'fits/playlists_songs_300D/dict_id_ntitle_final.csv'
+
+w2v_model = Word2Vec.load(emb_file_name)
+embeddings = []
+
+# write labels and get embeddings
+i = 0
+out_dir = type
+if not os.path.isdir(out_dir):
+    os.makedirs(out_dir)
+# write labels
+with open(out_dir + '/'+type+'_metadata.tsv', 'w') as f:
+    with open(dict_file_name, 'r') as dict_file:
+        dictionary_reader = csv.reader(dict_file, delimiter=',')
+        for row in dictionary_reader:
+            if row[0] != 'id':
+                if str(i) != row[0]:
+                    print(i)
+                i += 1
+                line_out = "%s\n" % row[1]
+                f.write(line_out)
+                embeddings.append(w2v_model.wv.word_vec(row[0]))
+embeddings = np.array(embeddings)
+
+# build the tf graph
 tf.reset_default_graph()
 sess = tf.InteractiveSession()
 X = tf.Variable([0.0], name='embedding')
 place = tf.placeholder(tf.float32, shape=embeddings.shape)
 set_x = tf.assign(X, place, validate_shape=False)
 
+# run it with the learned embeddings
 sess.run(tf.global_variables_initializer())
 sess.run(set_x, feed_dict={place: embeddings})
 
 
-# write labels
-with open(out_dir + '/playlists_songs_300D/'+type+'_metadata.tsv', 'w') as f:
-    line_out = "title\n"
-    f.write(line_out)
-    with open('fits/playlists_songs_300D/dict_id_ntitle.csv', 'r') as dict_file:
-        dictionary_reader = csv.reader(dict_file, delimiter=',', quotechar='|')
-        for row in dictionary_reader:
-            if row[0] != 'id':
-                line_out = "%s\n" % row[1]
-                f.write(line_out)
-
 # create a TensorFlow summary writer
-#summary_writer = tf.summary.FileWriter(out_dir + '/' + 'filtered_emb_viz.log', sess.graph)
-summary_writer = tf.summary.FileWriter(out_dir + '/' + type+'_emb_viz.log', sess.graph)
+summary_writer = tf.summary.FileWriter(out_dir, sess.graph)
 config = projector.ProjectorConfig()
 embedding_conf = config.embeddings.add()
 embedding_conf.tensor_name = 'embedding:0'
-embedding_conf.metadata_path = os.path.join(out_dir, '../../playlists_songs_300D/'+type+'_metadata.tsv')
+embedding_conf.metadata_path = os.path.join(out_dir, '../'+type+'_metadata.tsv')
 projector.visualize_embeddings(summary_writer, config)
 
 saver = tf.train.Saver()
